777f2f5524ee5cbe75c3f311825a76221554e54c
icqt <247640408@qq.com>
2022-09-30 21:04:58 +0800

    Add files via upload

---
+++ conf.php
@@ -0,0 +1,18 @@
+<?php
+return array (
+    'db' => array (
+        'type' => 'pdo_mysql',
+        'pdo_mysql' => array (
+            'master' => array (
+                'host' => 'localhost',
+                'user' => 'root',
+                'password' => 'root',
+                'name' => 'dbname',
+                'tablepre' => '',
+                'charset' => 'utf8',
+                'engine' => 'myisam',
+            ),
+            'slaves' => array (),
+        ),
+    ),
+);
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+$conf = include 'conf.php'; //配置文件
+include 'xiunophp/xiunophp.min.php'; //全局引用xiunophp
+echo '你好XIUNOPHP!';
